5d77a9df6d1de4e22268055b9d0a13ec457eb7fc
EnisOzer <enis.ozer@ug.bilkent.edu.tr>
2024-11-08 23:48:50 +0200

    commit message

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
-import pytest
+from fastapi import FastAPI
 
-print("hello")
+app = FastAPI()
+
+@app.post("/topic")
+def create_topic_handler():
+    return {"Hello": "World"}
+
+@app.post("/comment")
+def read_root():
+    return {"Hello": "World"}
+
+@app.put("/vote")
+def read_root():
+    return {"Hello": "World"}
