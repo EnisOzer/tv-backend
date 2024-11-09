c19f8512e8893f0c4acd804255cce86fd472e1bf
Uros Stankovic <urosboy2000@gmail.com>
2024-11-09 12:54:18 +0100

    Add timestamp to database entitities

--- src/database/init.sql
+++ src/database/init.sql
@@ -13,7 +13,8 @@ CREATE SCHEMA truevoice;
 -- Create Topic table
 CREATE TABLE truevoice.topic (
     topic_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),  -- Unique identifier for topic
-    topic_name VARCHAR(255) NOT NULL                      -- Topic name with a max length of 255 characters
+    topic_name VARCHAR(255) NOT NULL,                     -- Topic name with a max length of 255 characters
+    created_at TIMESTAMP WITH TIME ZONE                   -- Exact timestamp when topic was created
 );
 
 -- Create Comment table
@@ -21,7 +22,8 @@ CREATE TABLE truevoice.comment (
     comment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),  -- Unique identifier for comment
     topic_id UUID REFERENCES truevoice.topic(topic_id) ON DELETE CASCADE,  -- Foreign key to Topic
     content TEXT NOT NULL,                                   -- Content of the comment
-    creator_id UUID NOT NULL                                 -- Identifier for the comment's creator
+    creator_id UUID NOT NULL,                                -- Identifier for the comment's creator
+    created_at TIMESTAMP WITH TIME ZONE                      -- Exact timestamp when comment is posted
 );
 
 -- Create Vote table
