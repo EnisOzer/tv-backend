d54827e906b33c04aa73e991056e148de0db1b8c
Uros Stankovic <urosboy2000@gmail.com>
2024-11-09 14:24:24 +0100

    Add user to database

--- src/database/init.sql
+++ src/database/init.sql
@@ -10,6 +10,18 @@ CREATE DATABASE truevoice;
 -- Create schema
 CREATE SCHEMA truevoice;
 
+-- There are 2 roles of user in a system, standard and moderator
+CREATE TYPE user_role AS ENUM ('standard', 'moderator');
+
+
+-- Crate user
+-- User is used only to save history of activities if user wants to have that feature
+CREATE TABLE truevoice.user (
+    username VARCHAR(255) PRIMARY KEY,     -- User name, can be any string, mail is not allowed
+    role user_role DEFAULT 'standard',  -- Role of user
+    password VARCHAR(255) NOT NULL      -- Password of user
+);
+
 -- Create Topic table
 CREATE TABLE truevoice.topic (
     topic_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),  -- Unique identifier for topic
@@ -18,10 +30,12 @@ CREATE TABLE truevoice.topic (
 
 -- Create Comment table
 CREATE TABLE truevoice.comment (
-    comment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),  -- Unique identifier for comment
+    comment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),                 -- Unique identifier for comment
     topic_id UUID REFERENCES truevoice.topic(topic_id) ON DELETE CASCADE,  -- Foreign key to Topic
-    content TEXT NOT NULL,                                   -- Content of the comment
-    creator_id UUID NOT NULL                                 -- Identifier for the comment's creator
+    user_id VARCHAR(255) REFERENCES truevoice.user(username) ON DELETE SET NULL,  -- Foreign key to Topic
+    content TEXT NOT NULL,                                                 -- Content of the comment
+    approved BOOLEAN DEFAULT 'false',                                      -- Indicates whether comment is approved by moderator
+    rejected BOOLEAN DEFAULT 'false'                                       -- Indicates whether comment is rejected by moderator
 );
 
 -- Create Vote table
