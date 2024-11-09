972cc55d6297835eaf164db4f1583f597a464b58
Uros Stankovic <urosboy2000@gmail.com>
2024-11-09 14:43:38 +0100

    Use session id in comments

--- src/database/init.sql
+++ src/database/init.sql
@@ -10,15 +10,10 @@ CREATE DATABASE truevoice;
 -- Create schema
 CREATE SCHEMA truevoice;
 
--- There are 2 roles of user in a system, standard and moderator
-CREATE TYPE user_role AS ENUM ('standard', 'moderator');
-
-
--- Crate user
+-- Create user
 -- User is used only to save history of activities if user wants to have that feature
-CREATE TABLE truevoice.user (
-    username VARCHAR(255) PRIMARY KEY,     -- User name, can be any string, mail is not allowed
-    role user_role DEFAULT 'standard',  -- Role of user
+CREATE TABLE truevoice.moderator (
+    username VARCHAR(255) PRIMARY KEY,  -- User name, can be any string, mail is not allowed
     password VARCHAR(255) NOT NULL      -- Password of user
 );
 
@@ -26,14 +21,15 @@ CREATE TABLE truevoice.user (
 CREATE TABLE truevoice.topic (
     topic_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),  -- Unique identifier for topic
     topic_name VARCHAR(255) NOT NULL,                     -- Topic name with a max length of 255 characters
-    created_at TIMESTAMP WITH TIME ZONE                   -- Exact timestamp when topic was created
+    created_at TIMESTAMP WITH TIME ZONE,                  -- Exact timestamp when topic was created
+    moderator_id VARCHAR(255) REFERENCES truevoice.moderator(username) -- Moderator username who created this topic
 );
 
 -- Create Comment table
 CREATE TABLE truevoice.comment (
     comment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),                 -- Unique identifier for comment
     topic_id UUID REFERENCES truevoice.topic(topic_id) ON DELETE CASCADE,  -- Foreign key to Topic
-    user_id VARCHAR(255) REFERENCES truevoice.user(username) ON DELETE SET NULL,  -- Foreign key to Topic
+    user_session_id VARCHAR(15) NOT NULL,                                  -- Session id
     content TEXT NOT NULL,                                                 -- Content of the comment
     approved BOOLEAN DEFAULT 'false',                                      -- Indicates whether comment is approved by moderator
     rejected BOOLEAN DEFAULT 'false',                                      -- Indicates whether comment is rejected by moderator
